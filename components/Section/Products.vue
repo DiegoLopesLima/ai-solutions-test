d0b93bfe18ac51aded7d5d3e34ac59a4f2fdec02
Diego Lopes Lima <web.diego.lima@gmail.com>
2023-07-28 02:43:22 -0300

    feat: products integration

--- components/Section/Products.vue
+++ components/Section/Products.vue
@@ -1,21 +1,29 @@
 <template>
-  <article class="py-4">
+  <article id="products-section" class="py-4">
     <PageContainer>
       <PageTitle icon="mdi:apps">Produtos</PageTitle>
 
       <div
         class="flex flex-wrap gap-4 md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
       >
-        <div v-for="item in 7" :key="item" class="inline-flex flex-col gap-1">
-          <img src="https://dummyimage.com/615x345/ef4444/ffffff" alt="" />
+        <div
+          v-for="item in data"
+          :key="item.id"
+          class="inline-flex flex-col gap-1"
+        >
+          <img :src="item.image" alt="" />
 
-          <h3 class="font-semibold">Lorem ipsum</h3>
+          <h3 class="font-semibold">{{ item.name }}</h3>
 
           <div>
-            <p>Lorem ipsum dolor sit amet.</p>
+            <p>{{ item.description }}</p>
           </div>
         </div>
       </div>
     </PageContainer>
   </article>
 </template>
+
+<script lang="ts" setup>
+const { data } = await useFetch("/api/products");
+</script>
